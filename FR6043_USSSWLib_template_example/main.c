2fbb1acd86f1108fa12d82f1338a29e8aabc5d8e
gcrisler <gcrisler@gcrisler>
2019-08-09 09:13:29 -0500

    delete bloat in main and pre

--- FR6043_USSSWLib_template_example/main.c
+++ FR6043_USSSWLib_template_example/main.c
@@ -1,36 +1,4 @@
-/* --COPYRIGHT--,BSD
- * Copyright (C) 2017 Texas Instruments Incorporated - http://www.ti.com/
- *
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *    Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- *    Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the
- *    distribution.
- *
- *    Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * --/COPYRIGHT--*/
+
 #include <msp430.h>
 #include "ussSwLib.h"
 #include "USS_Config/USS_userConfig.h"
@@ -199,17 +167,7 @@ int main(void)
         checkCode(code, USS_message_code_valid_results);
 
 
-#if (APPLICATION_ENABLE_CHANNEL_SWAP == true)
-        if(appSwapInterval == 0)
-        {
-            code = USS_swapCaptureChannels(&gUssSWConfig);
-            code = USS_swapAlgorithmsCaptureBuffers(&gUssSWConfig);
 
-            appSwapInterval = APPLICATION_CHANNEL_SWAP_INTERVAL;
-        }else{
-            appSwapInterval--;
-        }
-#endif
 
         // Generate a 1 sec LPM3 delay
         USS_generateLPMDelay(&gUssSWConfig,
